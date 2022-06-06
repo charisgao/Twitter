ed4ec67e47a2cc63b06805af51d2e9047439de7e
charisgao <charis.m.gao@gmail.com>
2022-06-06 16:47:13 -0700

    compose

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -4,8 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
@@ -24,6 +30,7 @@ public class TimelineActivity extends AppCompatActivity {
 
     TwitterClient client;
     RecyclerView rvTweets;
+    Button btnLogout;
     List<Tweet> tweets;
     TweetsAdapter adapter;
 
@@ -34,8 +41,17 @@ public class TimelineActivity extends AppCompatActivity {
 
         client = TwitterApp.getRestClient(this);
 
-        // Find the RecyclerView
+        // Find the RecyclerView and log out button
         rvTweets = findViewById(R.id.rvTweets);
+        btnLogout = findViewById(R.id.btnLogout);
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onLogoutButton();
+                Toast.makeText(getApplicationContext(), "Logout successful", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         // Initialize the list of tweets and adapter
         tweets = new ArrayList<>();
@@ -46,6 +62,24 @@ public class TimelineActivity extends AppCompatActivity {
         rvTweets.setAdapter(adapter);
 
         populateHomeTimeline();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {   // item is the MenuItem that is tapped
+        // Handle presses on the action bar items
+        if (item.getItemId() == R.id.compose) { // Compose icon has been selected
+            // Navigate to the compose activity
+            Intent intent = new Intent(this, ComposeActivity.class);
+            startActivity(intent);
+        }
+        return true;
     }
 
     private void populateHomeTimeline() {
@@ -67,5 +101,16 @@ public class TimelineActivity extends AppCompatActivity {
                 Log.e(TAG, "onFailure" + response, throwable);
             }
         });
+    }
+
+    void onLogoutButton() {
+        // Forget who's logged in
+        TwitterApp.getRestClient(this).clearAccessToken();
+
+        // Navigate backwards to Login screen
+        Intent i = new Intent(this, LoginActivity.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // Makes sure the Back button won't work
+        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); // Same as above
+        startActivity(i);
     }
 }
